52c842368489f4e13922af5ac146c2b25f215eef
mapachurro <oliver.renwick@consensys.net>
2024-07-22 13:08:54 -0400

    fixing url path etc. for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/web3-glossary-website/', // Set this to your repository name
   plugins: [react()],
   resolve: {
     alias: {
